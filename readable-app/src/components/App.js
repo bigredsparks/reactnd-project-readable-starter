80727edb018ccf2f7d090165233fa09f3834e34f
Vince Sparks <svsparks@swbell.net>
2018-02-13 14:57:35 -0600

    feat: added moment.js for date/time formatting and create simply list of posts" "

--- readable-app/src/components/App.js
+++ readable-app/src/components/App.js
@@ -6,11 +6,48 @@ import PostDetail from './PostDetail'
 import PostEdit from './PostEdit'
 
 class App extends Component {
+  state = {
+    posts: [
+      {
+        id: '1',
+        timestamp: Date.now(),
+        title: 'Title 1',
+        body: 'Body 1',
+        author: 'Author Name',
+        category: 'React',
+        voteScore: 1,
+        deleted: false
+      },
+      {
+        id: '2',
+        timestamp: Date.now(),
+        title: 'Title 2',
+        body: 'Body 2',
+        author: 'Author Name',
+        category: 'Redux',
+        voteScore: 1,
+        deleted: false
+      },
+      {
+        id: '3',
+        timestamp: Date.now(),
+        title: 'Title 3',
+        body: 'Body 3',
+        author: 'Author Name',
+        category: 'Udacity',
+        voteScore: 1,
+        deleted: false
+      },
+    ]
+  }
+
   render() {
+    const { posts } = this.state
+
     return (
       <div className="App">
         <Route exact path='/' render={() =>(
-          <ListPosts
+          <ListPosts posts={posts}
           />
         )}/>
         <Route path='/category' render={({history}) =>(
--- readable-app/src/components/ListPosts.js
+++ readable-app/src/components/ListPosts.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import moment from 'moment'
 
 class ListPosts extends Component {
+
+  timestampToStr = (timestamp) => {
+    return moment(timestamp).format("MM/DD/YYYY h:mm:ss a")
+  }
+
   render() {
+    const { posts } = this.props
+
     return (
       <div className="ListPosts">
         <div className="categories">
@@ -18,9 +26,13 @@ class ListPosts extends Component {
           Posts
           <div className="post-list">
           <ul>
-              <li>Post 1</li>
-              <li>Post 2</li>
-              <li>Post 3</li>
+            {posts.map((post) => (
+              <li key={post.id}>
+                <div>
+                  {this.timestampToStr(post.timestamp)}: category:{post.category} author:{post.author} - {post.title} - {post.body}
+                </div>
+              </li>
+            ))}
             </ul>
           </div>
         </div>
